789c88c6eba89172d37cd0c2448fb07ed65b1d48
legop3 <minecratfd8@gmail.com>
2022-06-14 00:30:20 -0400

    fadsf

--- scheduler.js
+++ scheduler.js
@@ -40,7 +40,7 @@ async function monday() {
 
 //  0 0 * * FRI
 //every friday at midnight
-cron.schedule('*/2 * * * *', () => {
+cron.schedule('32 * * * *', () => {
   console.log('its friday and i');
   //move edited quest files into the folder
   //    cp /home/brody/eventquests/* /home/brody/serverfiles/Erpue/bin/quests
@@ -52,7 +52,7 @@ cron.schedule('*/2 * * * *', () => {
 
 //  0 0 * * MON
 //every monday at midnight
-cron.schedule('*/4 * * * *', () => {
+cron.schedule('33 * * * *', () => {
     console.log('its monday and i');
     //move original quests into quest folder
     //    cp /home/brody/normal/* /home/brody/serverfiles/Erpue/bin/quests
