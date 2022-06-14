8e90f538db32230b1ff5b3fd70dc38b3f49323c6
legop3 <minecratfd8@gmail.com>
2022-06-13 23:09:55 -0400

    fasdf

--- scheduler.js
+++ scheduler.js
@@ -1,6 +1,6 @@
 var cron = require('node-cron');
 var sudo = require('sudo-js');
-const { exec } = require('child_process');
+const { exec, execFile, execSync } = require('child_process');
 //also install module "uuid"
 
 
@@ -24,19 +24,26 @@ exec("konsole")
 
 
 
+async function friday() {
+  execSync("tmuxinator stop server")
+  execSync("cp /home/brody/eventquests/* /home/brody/serverfiles/Erpue/bin/quests -v")
+  execSync("tmuxinator start server")
+}
 
-
-
+async function monday() {
+  execSync("tmuxinator stop server")
+  execSync("cp /home/brody/normal/* /home/brody/serverfiles/Erpue/bin/quests -v")
+  execSync("tmuxinator start server")
+}
 
 
 //  0 0 * * FRI
 //every friday at midnight
-cron.schedule('0 0 * * FRI', () => {
+cron.schedule('*/2 * * * *', () => {
   console.log('its friday and i');
   //move edited quest files into the folder
   //    cp /home/brody/eventquests/* /home/brody/serverfiles/Erpue/bin/quests
-  exec("cp /home/brody/eventquests/* /home/brody/serverfiles/Erpue/bin/quests -v")
-
+  friday()
 
 
 });
@@ -44,12 +51,12 @@ cron.schedule('0 0 * * FRI', () => {
 
 //  0 0 * * MON
 //every monday at midnight
-cron.schedule('0 0 * * MON', () => {
+cron.schedule('1/2 * * * *', () => {
     console.log('its monday and i');
     //move original quests into quest folder
     //    cp /home/brody/normal/* /home/brody/serverfiles/Erpue/bin/quests
-    exec("cp /home/brody/normal/* /home/brody/serverfiles/Erpue/bin/quests -v")
-  
+    // exec("cp /home/brody/normal/* /home/brody/serverfiles/Erpue/bin/quests -v")
+    monday()
   
     
   });
