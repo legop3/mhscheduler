09234043315d742d8ef5fa83ea431a4b02d37bc3
legop3 <minecratfd8@gmail.com>
2022-06-13 21:54:06 -0400

    fdsa

--- scheduler.js
+++ scheduler.js
@@ -1,10 +1,14 @@
 var cron = require('node-cron');
 var sudo = require('sudo-js');
+const { exec } = require('child_process');
 //also install module "uuid"
-sudo.setPassword('1fads23');
 
 
+sudo.setPassword('123');
 
+
+// /home/brody/serverfiles/Erupe/bin/quests 
+//is the folder to movec quests into
 
 
 sudo.check(function(valid) {
@@ -16,6 +20,7 @@ sudo.check(function(valid) {
 
 
 
+exec("konsole")
 
 
 
@@ -24,25 +29,26 @@ sudo.check(function(valid) {
 
 
 
-
-
+//  0 0 * * FRI
 //every friday at midnight
 cron.schedule('0 0 * * FRI', () => {
   console.log('its friday and i');
-
-
+  //move edited quest files into the folder
+  //    cp /home/brody/eventquests/* /home/brody/serverfiles/Erpue/bin/quests
+  exec("cp /home/brody/eventquests/* /home/brody/serverfiles/Erpue/bin/quests -v")
 
 
 
 });
 
 
-
+//  0 0 * * MON
 //every monday at midnight
 cron.schedule('0 0 * * MON', () => {
     console.log('its monday and i');
-    
-  
+    //move original quests into quest folder
+    //    cp /home/brody/normal/* /home/brody/serverfiles/Erpue/bin/quests
+    exec("cp /home/brody/normal/* /home/brody/serverfiles/Erpue/bin/quests -v")
   
   
     
