cfec9e39d1c8eebc5aa0fc880cc2114720e2c4a1
lucky0wl-sketch <Brodywelch9@gmail.com>
2022-09-25 20:19:00 -0400

    5

--- scheduler.js
+++ scheduler.js
@@ -3,7 +3,7 @@ var cron = require('node-cron');
 const { exec, execFile, execSync } = require('child_process');
 const { stderr, stdout } = require('process');
 const { Client, Intents, MessageEmbed } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { client } = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const fs = require('fs');
 //also install module "uuid"
 
